049e70d9be508caac7f0b986b82710091f86b431
alastifer <grishkevichaaa@gmail.com>
2017-03-04 23:03:33 +0300

    add equals() and hashcode() for work with linkedHashMap

--- src/main/java/com/alastifer/shop/entity/Product.java
+++ src/main/java/com/alastifer/shop/entity/Product.java
@@ -6,7 +6,7 @@ public class Product {
 
     private final int id;
 
-    public Product(String name, int id) {
+    public Product(final String name, final int id) {
         this.name = name;
         this.id = id;
     }
@@ -17,6 +17,24 @@ public class Product {
 
     public int getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != product.id) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
     }
 
 }
