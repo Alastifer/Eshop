3544db508c6849a00a56bb842f5903e3fbd9247d
alastifer <grishkevichaaa@gmail.com>
2017-03-04 23:05:15 +0300

    controller for add product in basket

---
+++ src/main/java/com/alastifer/shop/controller/ProductAddToBasketController.java
@@ -0,0 +1,56 @@
+package com.alastifer.shop.controller;
+
+import com.alastifer.shop.dao.ProductsDAO;
+import com.alastifer.shop.dao.exception.NoSuchEntityException;
+import com.alastifer.shop.entity.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@WebServlet("/productAdd.do")
+public class ProductAddToBasketController extends HttpServlet {
+
+    private static final String ATTRIBUTE_PRODUCT_BASKET = "productBasket";
+
+    private static final String PARAM_ID = "id";
+
+    private final ProductsDAO productsDAO = new ProductsDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idProduct = req.getParameter(PARAM_ID);
+
+        if (idProduct != null) {
+            try {
+                Product product = productsDAO.getProductByID(Integer.valueOf(idProduct));
+                HttpSession session = req.getSession();
+
+                Map<Product, Integer> oldBasket = (Map<Product, Integer>) session.getAttribute(ATTRIBUTE_PRODUCT_BASKET);
+
+                if (oldBasket == null) {
+                    session.setAttribute(ATTRIBUTE_PRODUCT_BASKET, Collections.singletonMap(product, 1));
+                } else {
+                    Map<Product, Integer> newBasket = new LinkedHashMap<>(oldBasket);
+                    if (!newBasket.containsKey(product)) {
+                        newBasket.put(product, 1);
+                    } else {
+                        newBasket.put(product, newBasket.get(product) + 1);
+                    }
+
+                    session.setAttribute(ATTRIBUTE_PRODUCT_BASKET, Collections.unmodifiableMap(newBasket));
+                }
+
+                resp.sendRedirect("product.do?id=" + idProduct);
+            } catch (NoSuchEntityException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
