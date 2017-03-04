7b3a210994402c2dec253ff1561a4700faf3e657
alastifer <grishkevichaaa@gmail.com>
2017-03-04 17:05:53 +0300

    Created entity "Product"

---
+++ src/main/java/com/alastifer/shop/entity/Product.java
@@ -0,0 +1,22 @@
+package com.alastifer.shop.entity;
+
+public class Product {
+
+    private final String name;
+
+    private final int id;
+
+    public Product(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
