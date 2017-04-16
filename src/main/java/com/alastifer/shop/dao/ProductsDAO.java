b7078c81bdcffe38b72fec90641afaf09bcc04c2
alastifer <grishkevichaaa@gmail.com>
2017-04-16 11:55:57 +0300

    refactor dao all information about products storage in database

--- src/main/java/com/alastifer/shop/dao/ProductsDAO.java
+++ src/main/java/com/alastifer/shop/dao/ProductsDAO.java
@@ -1,47 +1,70 @@
 package com.alastifer.shop.dao;
 
+import com.alastifer.shop.dao.exception.DAOException;
 import com.alastifer.shop.dao.exception.NoSuchEntityException;
 import com.alastifer.shop.entity.Product;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.*;
 
 public class ProductsDAO {
 
-    private final List<Product> database = new ArrayList<>();
+    private static final String QUERY_SELECT_ALL = "SELECT id, name FROM products";
 
-    public ProductsDAO() {
-        database.add(new Product("Sugar", 1));
-        database.add(new Product("Bread", 2));
-        database.add(new Product("Paper", 3));
-        database.add(new Product("Pen", 4));
-    }
+    private static final String QUERY_SELECT_BY_ID = "SELECT id, name FROM products WHERE id=?";
 
-    public Product getProductByID(final Integer id) throws NoSuchEntityException {
-        if (id > database.size() || id <= 0) {
-            throw new NoSuchEntityException("No element with ID " + id + " in database");
-        }
+    public Product getProductByID(final Integer id) throws NoSuchEntityException, DAOException {
+        DataSource dataSource = initDataSource();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pStatement = connection.prepareStatement(QUERY_SELECT_BY_ID)){
 
-        Product product = database.get(id - 1);
+            pStatement.setInt(1, id);
+            ResultSet resultSet = pStatement.executeQuery();
 
-        if (product == null) {
-            throw new NoSuchEntityException("No element with ID " + id + " in database");
-        }
-
-        return product;
-    }
-
-    public Product getProductByName(final String productName) throws NoSuchEntityException {
-        for (Product product : database) {
-            if (product.getName().equals(productName)) {
-                return product;
+            if (resultSet.next()) {
+                String name = resultSet.getString(2);
+                return new Product(name, id);
+            } else {
+                throw new NoSuchEntityException("No product with id " + id);
             }
-        }
 
-        throw new NoSuchEntityException("No element with name \"" + productName + "\" in database");
+        } catch (SQLException e) {
+            throw new DAOException("Error in database " + e.getMessage(), e);
+        }
     }
 
-    public List<Product> getAllProducts() {
-        return database;
+    public List<Product> getAllProducts() throws DAOException {
+        List<Product> products = new ArrayList<>();
+
+        DataSource dataSource = initDataSource();
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(QUERY_SELECT_ALL)) {
+
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                products.add(new Product(name, id));
+            }
+
+        } catch (SQLException e) {
+            throw new DAOException("Error in database " + e.getMessage(), e);
+        }
+
+        return products;
+    }
+
+    private DataSource initDataSource() throws DAOException {
+        try {
+            Context context = new InitialContext();
+            return  (DataSource) context.lookup("java:comp/env/jdbc/eShop");
+        } catch (NamingException e) {
+            throw new DAOException("Error in initialContext: " + e.getMessage(), e);
+        }
     }
 
 }
